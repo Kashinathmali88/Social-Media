731431fe01f32dba69fb89c2ecfd67bc610c84ba
kashinath mali <kashinathmali88@gmail.com>
2025-09-21 11:12:47 +0530

    fix inngest error

--- backend/src/inngest/index.js
+++ backend/src/inngest/index.js
@@ -1,10 +1,10 @@
 import { Inngest } from "inngest";
 import User from "../models/user.model.js";
+import "dotenv/config";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({
   id: "pingUp-app",
-  eventKey: process.env.INNGEST_EVENT_KEY,
   signingKey: process.env.INNGEST_SIGNING_KEY,
 });
 
