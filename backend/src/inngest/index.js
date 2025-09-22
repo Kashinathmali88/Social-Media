7142110f8ab3b50209711231772bd4f38252c2f2
kashinath mali <kashinathmali88@gmail.com>
2025-09-22 16:58:42 +0530

    final inngest adddec

--- backend/src/inngest/index.js
+++ backend/src/inngest/index.js
@@ -26,7 +26,7 @@ const syncUserCreation = inngest.createFunction(
     const userData = {
       _id: id,
       email: email_addresses[0].email_addresses,
-      full_naem: first_name + " " + last_name,
+      full_name: first_name + " " + last_name,
       profile_picture: image_url,
       username: username,
     };
