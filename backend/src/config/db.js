a3cfed9434fac81a388bb8f295f293fb5b69ffaf
kashinath mali <kashinathmali88@gmail.com>
2025-10-02 12:18:47 +0530

    new db code because mongo insert.One errer

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,22 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  // try {
-  //   mongoose.connection.on("connected", () => console.log("DB connected"));
-  //   await mongoose.connect(`${process.env.MONGO_URI}/social_media`);
-  // } catch (error) {
-  //   console.log(error.message);
-  // }
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-      console.log("DB connected");
-    })
-    .catch((err) => {
-      console.log(err);
-
-      console.log("Faild to connect");
-    });
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/social_media`);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log("Failed to connect", error);
+  }
 };
 
 export default connectDB;
